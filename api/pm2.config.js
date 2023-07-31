830b27e7704d46b844f97b59015287fe17f3add3
Lad Ab <laddev.site@inbox.lv>
2023-07-31 15:01:00 +0300

    Bug fixed

--- api/pm2.config.js
+++ api/pm2.config.js
@@ -4,13 +4,13 @@ module.exports = {
             name: 'Api',
             script: 'app.js',
             watch: true,
-            ignore_watch: ['node_modules', 'logs', "db"],
+            ignore_watch: ['node_modules', 'logs', "db", "tmp"],
         },
         {
             name: 'HandshakeMonitor',
             script: 'handshake_monitor.js',
             watch: true,
-            ignore_watch: ['node_modules', 'logs', "db"],
+            ignore_watch: ['node_modules', 'logs', "db", "tmp"],
         },
     ],
 };
